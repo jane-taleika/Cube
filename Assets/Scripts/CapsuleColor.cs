bdf1476c2a8e62c6fc35558c35fb0899ce9d8a4d
Kostyshina <i.vote.saxon@gmail.com>
2015-11-05 00:13:28 +0300

    Add new object Capsule with changing color

---
+++ Assets/Scripts/CapsuleColor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CapsuleColor : MonoBehaviour {
+
+    void Awake()
+    {
+        Debug.Log("Capsule and Cube movements");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
