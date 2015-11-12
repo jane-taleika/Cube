b6ccf3e52cad8252b55147a1a39407691328e107
Kostyshina <i.vote.saxon@gmail.com>
2015-11-12 15:52:23 +0300

    Add move script for future pacman

---
+++ Assets/Practice2(Pacman)/Scripts/SphereMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMove : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Let's go future pacman");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = speed * Input.GetAxis("Horizontal") + transform.position.x;
+        if (xAxis != 0)
+        {
+            transform.position = new Vector3(xAxis, transform.position.y, transform.position.z);
+        }
+    }
+}
