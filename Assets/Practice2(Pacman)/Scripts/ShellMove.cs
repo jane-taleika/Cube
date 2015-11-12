9c53909eee1db041bf39ea06abd8d1b2af8769a3
Kostyshina <i.vote.saxon@gmail.com>
2015-11-12 23:58:51 +0300

    Changed pacman position and destroy function

--- Assets/Practice2(Pacman)/Scripts/ShellMove.cs
+++ Assets/Practice2(Pacman)/Scripts/ShellMove.cs
@@ -7,15 +7,20 @@ public class ShellMove : MonoBehaviour {
     public float speed;
 
     // Use this for initialization
-    void Start () {
-        Debug.Log("pew");
-	}
+    void Start()
+    {
+        if (gameObject.name == "Shell(Clone)")
+        {
+            Debug.Log("pew");
+            Destroy(gameObject, lifeTime);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (gameObject.name == "Shell(Clone)"){
-            transform.Translate(new Vector3(1.0f, 0.0f, 0.0f) * speed);
-            Destroy(gameObject, lifeTime);
-        }
+            if (gameObject.name == "Shell(Clone)")
+            {
+                transform.Translate(new Vector3(1.0f, 0.0f, 0.0f) * speed);
+            }
     }
 }
