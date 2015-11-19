dd725cb28e183f037251e2f788361f81e5337062
Kostyshina <i.vote.saxon@gmail.com>
2015-11-19 14:49:47 +0300

    Visible laser

---
+++ Assets/Practice3/Scripts/LaserShoot.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserShoot : MonoBehaviour {
+
+    LineRenderer laserLine;
+    public float shootRange = 25f;
+    public GameObject star;
+
+    void Awake()
+    {
+        laserLine = GetComponent<LineRenderer>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(star, new Vector3(transform.position.x + 20f, transform.position.y, transform.position.z), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Start shooting");
+            Shoot();
+        }
+    }
+
+    private void Shoot()    //IEnumerator
+    {
+        laserLine.enabled = true;
+        laserLine.SetPosition(0, transform.position);
+        Ray ray = new Ray(transform.position, transform.parent.transform.right);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, shootRange))
+        {
+            laserLine.SetPosition(1, hit.point);
+            Destroy(hit.collider.gameObject);
+            Debug.Log("You've destroyed Death Star");
+            Instantiate(star, new Vector3(transform.position.x + 20f, transform.position.y, transform.position.z), Quaternion.identity);
+        }
+        else
+        {
+            laserLine.SetPosition(1, ray.origin + ray.direction * shootRange);
+        }
+        StartCoroutine(reset());
+    }
+
+    private IEnumerator reset() {
+        yield return new WaitForSeconds(0.05f);
+        laserLine.enabled = false;
+    }
+}
--- Assets/Practice3/Scripts/PacmanMoveStarCreate.cs
+++ Assets/Practice3/Scripts/PacmanMoveStarCreate.cs
@@ -3,15 +3,13 @@ using System.Collections;
 
 public class PacmanMoveStarCreate : MonoBehaviour {
 
-    private float dist = 20f;
+    
     public float speed;
-    public GameObject star;
-
+    
     // Use this for initialization
     void Start()
     {
         Debug.Log("Let's go pacman");
-        Instantiate(star, new Vector3(transform.position.x+10f, transform.position.y, transform.position.z),Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -22,26 +20,6 @@ public class PacmanMoveStarCreate : MonoBehaviour {
         {
             transform.position = new Vector3(xAxis, transform.position.y, transform.position.z);
         }
-        
-        //transform.Translate(new Vector3(xAxis, 0.0f, 0.0f));
-        if (Input.GetKeyDown("space"))
-        {
-            Shot();
-        }
     }
 
-    void Shot()
-    {
-        Ray ray = new Ray(transform.position, transform.right);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, dist))
-        {
-            Vector3 pos = new Vector3(transform.position.x + 5f, transform.position.y, transform.position.z);
-            Vector3 dir = transform.TransformDirection(Vector3.right) * 5;
-            Debug.DrawRay(pos, dir, Color.green, 2);
-            Destroy(hit.collider.gameObject);
-            Debug.Log("You've destroyed Death Star");
-            Instantiate(star, new Vector3(transform.position.x + 10f, transform.position.y, transform.position.z), Quaternion.identity);
-        }
-    }
 }
