4b6cff9994a78c1f299124af48631bcd24b7bd7d
leyataleya <leyataleya@gmail.com>
2015-11-25 20:46:22 +0300

    Shooting by z and x, Score counter

---
+++ Assets/Practice3/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+    public int score;
+    public Text ScoreText;
+
+    void Awake()
+    {
+        ScoreText = GetComponent<Text>();
+        score = 0;
+    }
+
+    void Update()
+    {
+        ScoreText.text = "Score:" + score;
+    }
+	
+}
