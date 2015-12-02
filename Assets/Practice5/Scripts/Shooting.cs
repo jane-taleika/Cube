1fccf61068926e4f2007eb70236a8fae583a4f0b
leyataleya <leyataleya@gmail.com>
2015-12-02 11:41:48 +0300

    random star appearance

--- Assets/Practice5/Scripts/Shooting.cs
+++ Assets/Practice5/Scripts/Shooting.cs
@@ -58,8 +58,14 @@ public class Shooting : MonoBehaviour {
 			Destroy(hit.collider.gameObject);
 			Debug.Log("You've destroyed Death Star");
 			ScoreIncrease.Score +=10;
-			//Player.GetComponent<ScoreManager>().score += 10;
-			Instantiate(star, new Vector3(transform.position.x + 15f, transform.position.y, transform.position.z), Quaternion.identity);
+			//Player.GetComponent<ScoreManager>().score += 10;          
+			//Instantiate(star, new Vector3(transform.position.x + 15f, transform.position.y, transform.position.z), Quaternion.identity);
+            float randomRange=Random.Range(-19.0F, 19.0F);
+            if ((transform.position.x + randomRange > transform.parent.transform.position.x + 2f) || (transform.position.x + randomRange < transform.parent.transform.position.x - 2f))
+            Instantiate(star, new Vector3(transform.position.x + randomRange, transform.position.y, transform.position.z), Quaternion.identity);
+            else
+                Instantiate(star, new Vector3(transform.position.x + 10f, transform.position.y, transform.position.z), Quaternion.identity);
+
 		}
 		else
 		{
